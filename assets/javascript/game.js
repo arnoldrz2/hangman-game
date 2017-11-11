3f454751c467269ffa3038b86be7fa795f083575
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2017-11-10 19:11:19 -0600

    js update-added for loop for blank spaces, keypress function, and lives number function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,31 +8,62 @@ var lives = 9;
 var getItem = function () {
 
 	// Video Games Titles
-	var titles = ["Mario Bros", "Final Fantasy", "Mortal Kombat", 
-						"Doom", "Pac Man", "Tetris", "Space Invaders", 
-						"Sonic the Hedgehog", "Donkey Kong", "Pong", 
-						"Street Fighter", "The Legend of Zelda",
-						"Frogger", "Mario Kart", "Asteroids",];
-	var possibleTitle = titles[Math.floor(Math.random() * words.legth)];
-
-  var originalLength = possibleTitle.legth;
+	var titles = ['Mario Bros', 'Final Fantasy', 'Mortal Kombat', 
+						'Doom'];
+	var possibleTitle = titles[Math.floor(Math.random() * titles.length)];
+  //Space out possibleWord
+  var originalLength = possibleTitle.length;
   for (i = 0; i < originalLength; i++) {
     possibleTitle = [possibleTitle.slice(0, i*2+1), ' ', 
     possibleTitle.slice(i*2+1)].join('');
   }
 
-  var blankSpaces 
+  var blankSpaces = "";
+  var titleLength = possibleTitle.length;
 
+  for (i = 0; i < titleLength; i++) {
+    var x = possibleTitle.charAt(i);
 
+    if (x = possibleTitle) {
+      blankSpaces += x;
+    }
+    else { 
+      blankSpaces += "_"
+    }
+  }
+  document.getElementById("blankSpaces").innerHTML = blankSpaces;
+};
 
-}					
+document.onkeypress = function(keyPressed) {
+  var keyPressed = keyPressed || window.event;
+  charCode = keyPressed.keyCode || keyPressed.which,
+  lettersGuessed = String.fromCharCode(charCode);
 
+document.getElementById("lettersGuessed").innerHTML += lettersGuessed;
+document.getElementById("lives").innerHTML = lives;
+lives--;
 
-
-
-
-
+if (lives === -1) {
+  alert("Game Over!")
+  }
 }
+
+getItem();
+
+
+
+
+
+
+
+
+})();
+					
+//Spare Video Game Title Bank
+//  'Pac Man', 'Tetris', 'Space Invaders', 
+//  'Sonic the Hedgehog', 'Donkey Kong', 'Pong', 
+//  'Street Fighter', 'The Legend of Zelda',
+//  'Frogger', 'Mario Kart', 'Asteroids'
 
 // Letters
 // var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
