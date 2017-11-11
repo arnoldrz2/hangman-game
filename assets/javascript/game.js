60cec9975f37ebbcc859aa5081e0d2e5e8864699
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2017-11-11 03:03:55 -0600

    updated lettersGuessed html write and worked userGuess var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,71 +10,93 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h",
 var lives = 9;
 var wins = 0;
 var blankSpaces = "";
-var lettersGuessed = [];
+
 
 (function(){
 
-// KeyPress Capture Function
-var getItem = function () {
+
+  var getItem = function () {
 
 
-	var possibleTitle = title[Math.floor(Math.random() * title.length)];
+  	var possibleTitle = title[Math.floor(Math.random() * title.length)];
 
-  //Space out possibleWord
-  var titleLength = possibleTitle.length;
-  for (i = 0; i < titleLength; i++) {
-    possibleTitle = [possibleTitle.slice(0, i*2+1), ' ', 
-    possibleTitle.slice(i*2+1)].join('');
-  }
-
-  // Displays possible words blank spaces
-  var wordLength = possibleTitle.length;
-  for (i = 0; i < wordLength; i++) {
-    var x = possibleTitle.charAt(i);
-
-    if (x === " " || x === "/'") {
-      blankSpaces += x;
+    //Space out possibleWord
+    var titleLength = possibleTitle.length;
+    for (i = 0; i < titleLength; i++) {
+      possibleTitle = [possibleTitle.slice(0, i*2+1), ' ', 
+      possibleTitle.slice(i*2+1)].join('');
     }
-    else { 
-      blankSpaces += "_";
-    }
-  }
-  document.getElementById("blank-spaces").innerHTML = blankSpaces;
 
+    // Displays possible words blank spaces
+    var wordLength = possibleTitle.length;
+    for (i = 0; i < wordLength; i++) {
+      var x = possibleTitle.charAt(i);
 
-  document.onkeypress = function(keyPressed) {
-    var keyPressed = keyPressed || window.event;
-    charCode = keyPressed.keyCode || keyPressed.which,
-    lettersGuessed = String.fromCharCode(charCode);
-
-  document.getElementById("letters-guessed").innerHTML += lettersGuessed;
-  document.getElementById("num-lives").innerHTML = lives;
-  lives--;
-
-
-  var userGuess = event.key.toLowerCase();
-
-  if (title.includes(userGuess)) {
-    for (var i = 0; i < possibleTitle.length; i++) {
-      if (userGuess == title[i]) {
-        blankSpaces[i] = userGuess;
+      if (x === " " || x === "/'") {
+        blankSpaces += x;
+      }
+      else { 
+        blankSpaces += "_";
       }
     }
-  }
+    document.getElementById("blank-spaces").innerHTML = blankSpaces;
+  };
+  
+
+
+
+
+    document.onkeypress = function(keyPressed) {
+      var keyPressed = keyPressed || window.event;
+      charCode = keyPressed.keyCode || keyPressed.which,
+      lettersGuessed = String.fromCharCode(charCode);
+
+
+      var userGuess
+        if (title.indexOf(userGuess) > -1) {
+         alert("Your guess is correct.")
+        }
+        // else {
+        //  alert("Your guess is wrong.")
+        // }
 
 
 
 
 
 
-  if (lives === -1) {
-    alert("Game Over!")
+    document.getElementById("letters-guessed").innerHTML += lettersGuessed + ", ";
+    document.getElementById("num-lives").innerHTML = lives;
+    lives--;
+
+
+
+
+
+
+
+
+    // var userGuess = event.key.toLowerCase();
+
+    // if (title.includes(userGuess)) {
+    //   for (var i = 0; i < possibleTitle.length; i++) {
+    //     if (userGuess == title[i]) {
+    //       blankSpaces[i] = userGuess;
+    //     }
+    //   }
+    // }
+
+
+
+
+
+
+    if (lives === -1) {
+      alert("Game Over!")
+      }
+
     }
-
-  }
-
-};
-getItem();
+    getItem();
 })();
 					
 //Spare Video Game Title Bank
