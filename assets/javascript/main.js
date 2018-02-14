a52454a47f7aaf984b2f0c8678f6a9c62b8ca0ed
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-02-14 17:25:15 -0600

    fixed small error, created userGuess var outside function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,20 @@
 window.onload = function() {
 // Array of Video Games Titles
-var titles = ["mariobros", "mortalkombat", 
-              "doom","donkeykong", "pong", "pacman", 
-              "tetris", "spaceinvaders", "sonicthehedgehog", 
-              "finalfantasy", "streetfighter", 
+var titles = ["mariobros", "mortalkombat",
+              "doom","donkeykong", "pong", "pacman",
+              "tetris", "spaceinvaders", "sonicthehedgehog",
+              "finalfantasy", "streetfighter",
               "thelegendofzelda", "frogger", "mariokart", "asteroids"];
 // Array of Letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h",
-               "i", "j", "k", "l", "m", "n", "o", "p", 
-               "q", "r", "s", "t", "u", "v", "w", "x", 
+               "i", "j", "k", "l", "m", "n", "o", "p",
+               "q", "r", "s", "t", "u", "v", "w", "x",
                "y", "z"];
 // Other Variables
 var lettersGuessed = "";
 var currentTitle = [];
 var alreadyGuessed = [];
+var userGuess = "";
 
 // alert("Press Enter to Begin!")
 
@@ -44,28 +45,18 @@ function newGame () {
       for (i=0; i < letters.length; i++){
         if (keyPress === letters[i]){
           userGuess = event.key.toLowerCase();
-          } 
+          }
         }
 
 
   if(title.includes(userGuess)) {
     for (i=0; i < title.length; i++) {
-      if (userGuess == title[i]) { 
+      if (userGuess == title[i]) {
         currentTitle[i] = userGuess;
         alreadyGuessed.push(userGuess);
       }
     }
 
-// && add some way to check if return if true or false. then act accordingly
-// 
-// for (i=0; i< alreadyGuessed.lenth; i++){
-//  if(userGuess === alreadyGuessed[i])
-//    do not push 
-// } put into function and say if userGuess === alreadyGuessed[i] 
-//    return true or assign true to a variable
-//  
-// 
-// 
     $('#current-title').html(currentTitle);
     $('#already-guessed').text(alreadyGuessed.join(', '));
   }
@@ -76,12 +67,6 @@ function newGame () {
     alreadyGuessed.push(userGuess);
     $('#already-guessed').text(alreadyGuessed.join(', '));
   }
-
-
- 
-
-
-
 
   if (currentTitle.join("") == title){
     alert("You Win!")
@@ -97,9 +82,11 @@ function newGame () {
 
   }
 }
+
+
+
+
+
+
 newGame();
 }
-
-
-// Extra Code
-// $('#already-guessed').html(alreadyGuessed);
